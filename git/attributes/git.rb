6f52e3642088e2763823a3b9648fdb964c782d15
Bob Burbach <bob.burbach@gmail.com>
2011-01-05 11:35:18 -0800

    Out of date software sucks. I'm looking at you apt! Install git from source.

---
+++ git/attributes/git.rb
@@ -0,0 +1,4 @@
+default_unless[:git][:version] = "1.7.3.4"
+default_unless[:git][:url]     = "http://kernel.org/pub/software/scm/git/git-#{node[:git][:version]}.tar.bz2"
+
+default_unless[:git][:install_path] = '/usr/local'
--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -16,11 +16,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-case node[:platform]
-when "debian", "ubuntu"
-  package "git-core"
-else 
-  package "git"
+remote_file "/tmp/git-#{node[:git][:version]}.tar.bz2" do
+  source "#{node[:git][:url]}"
+  not_if { ::File.exists?("/tmp/git-#{node[:git][:version]}.tar.bz2") }
 end
 
-package "git-email"
+bash "Install Git from Source" do
+  cwd "/tmp"
+  code <<-EOH
+  apt-get build-dep git-core -y
+  tar xvfj git-#{node[:git][:version]}.tar.bz2
+  cd git-#{node[:git][:version]}
+  make prefix=#{node[:git][:install_path]} install
+  EOH
+  not_if do
+    ::File.exists?("#{node[:git][:install_path]}/bin/git") &&
+    system("#{node[:git][:install_path]}/bin/git --version | grep -q '#{node[:git][:version]}'")
+  end
+end
